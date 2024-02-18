ffa2d3f4704d16a3ca981be959d452365e723051
hyperb0rean <jewellin02@gmail.com>
2024-02-18 13:20:15 +0300

    tg bot update

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,77 @@
 #include <tgbot/tgbot.h>
 
+#include <format>
 #include <iostream>
 
-int main() {
+const std::string GetCurrentTime() {
+    time_t now = time(0);
+    struct tm tstruct;
+    char buf[80];
+    tstruct = *localtime(&now);
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+    return buf;
+}
 
+bool IsAdmin(const std::string& username) {
+    std::vector<std::string> admins = {"hyperb0rean", "mopstream"};
+    for (const auto& admin : admins) {
+        if (username == admin) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    std::vector<std::string> bot_commands = {"start", "kill"};
+    bool is_alive = true;
     TgBot::Bot bot(std::getenv("TOKEN"));
     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "Hi!");
+        if (IsAdmin(message->from->username)) {
+            bot.getApi().sendMessage(message->chat->id,
+                                     std::format("Hello master {}!", message->from->username));
+
+        } else {
+            bot.getApi().sendMessage(message->chat->id, "Hello!");
+        }
     });
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-        printf("User wrote %s\n", message->text.c_str());
-        if (StringTools::startsWith(message->text, "/start")) {
-            return;
+    bot.getEvents().onCommand("kill", [&bot, &is_alive](TgBot::Message::Ptr message) {
+        if (IsAdmin(message->from->username)) {
+            bot.getApi().sendMessage(message->chat->id,
+                                     "Change the world my final message. Goodbye.......");
+            std::cerr << GetCurrentTime() << " | Killed\n";
+            is_alive = false;
+        }
+    });
+    bot.getEvents().onAnyMessage([&bot, &bot_commands](TgBot::Message::Ptr message) {
+        std::cerr << GetCurrentTime()
+                  << std::format(" | {} wrote {}\n", message->from->username, message->text);
+        for (const auto& command : bot_commands) {
+            if (StringTools::startsWith(message->text, "/" + command)) {
+                return;
+            }
         }
         bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
     });
+
+    signal(SIGINT, [](int s) {
+        std::cerr << GetCurrentTime() << "SIGINT got\n";
+        std::exit(0);
+    });
+
     try {
-        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        std::cerr << GetCurrentTime()
+                  << std::format(" | Bot username: {}\n", bot.getApi().getMe()->username);
         TgBot::TgLongPoll longPoll(bot);
         while (true) {
-            printf("Long poll started\n");
+            std::cerr << GetCurrentTime() << " | Long poll started\n";
             longPoll.start();
+            if (!is_alive) {
+                std::exit(0);
+            }
         }
     } catch (TgBot::TgException& e) {
-        printf("error: %s\n", e.what());
+        std::cerr << GetCurrentTime() << std::format(" | error: {}\n", e.what());
     }
     return 0;
 }
