2ceb59c7867cf09a89c80b6e20e38d227e4e06da
hyperb0rean <jewellin02@gmail.com>
2024-02-18 23:01:48 +0300

    addition

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 
 #include <tgbot/tgbot.h>
 
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
 #include <format>
 #include <fstream>
 #include <initializer_list>
@@ -23,6 +25,12 @@ public:
 bool HandleUser(size_t id, std::unordered_map<size_t, size_t>& users) {
     ++users[id];
     return users[id] > kMaxQueries;
+}
+
+std::vector<std::string> GetArguments(std::string message) {
+    std::vector<std::string> tokens;
+    boost::split(tokens, message, boost::is_any_of(" "));
+    return tokens;
 }
 
 std::vector<std::string> ReadDDLQueries(std::string filename) {
@@ -54,7 +62,7 @@ void InitDatabase() {
     for (const auto& ddl_query : ddl_queries) {
         pqxx::work query(c);
         try {
-            std::cerr << ddl_query << std::endl;
+            // std::cerr << ddl_query << std::endl;
             pqxx::result res = query.exec(ddl_query);
             query.commit();
         } catch (...) {
@@ -69,9 +77,22 @@ int main() {
 
     InitDatabase();
 
-    std::vector<Command> bot_commands = {{"start", "User greetings"},
-                                         {"kill", "Kills bot if you have permission"},
-                                         {"help", "Commands info"}};
+    std::vector<Command> bot_commands = {
+        {"start", "User greetings"},
+        {"kill", "Kills bot if you have permission"},
+        {"help", "Commands info"},
+        {"get_friend_email", "Get email of friends by name"},
+        {"add_crop", "Add new crop into database (string, percent, uint, bool, percent)"},
+        {"add_client", "Add new client into database"},
+        {"add_client_family_member", "Add new client family member into database"},
+        {"add_crops_plantation", "Add new crops plantation into database"},
+        {"add_estate", "Add new estate into database"},
+        {"add_evidence_info", "Add new evidence_info into database"},
+        {"add_landlord", "Add new landlord into database"},
+        {"add_manager", "Add new manager into database"},
+        {"add_plantation", "Add new plantation into database"},
+        {"add_worker", "Add new worker into database"},
+        {"add_worker_family_member", "Add new worker family member into database"}};
 
     std::unordered_map<size_t, size_t> users{};
 
@@ -130,6 +151,48 @@ int main() {
         }
         bot.getApi().sendMessage(message->chat->id, std::move(result));
     });
+    bot.getEvents().onCommand(
+        "get_friend_email", [&bot, &bot_commands, &users](TgBot::Message::Ptr message) {
+            if (HandleUser(message->chat->id, users)) {
+                return;
+            }
+            std::string result;
+            auto args = GetArguments(message->text);
+
+            pqxx::connection conn;
+            pqxx::work query(conn);
+            try {
+                pqxx::result res = query.exec(std::format(""));
+                query.commit();
+            } catch (...) {
+                std::cout << "Database: failed to execute query: " << message->text << std::endl;
+            }
+            bot.getApi().sendMessage(message->chat->id, std::move(result));
+        });
+
+    bot.getEvents().onCommand(
+        "add_crop", [&bot, &bot_commands, &users](TgBot::Message::Ptr message) {
+            if (HandleUser(message->chat->id, users)) {
+                return;
+            }
+            std::string result;
+            auto args = GetArguments(message->text);
+
+            pqxx::connection conn;
+            pqxx::work query(conn);
+            try {
+
+                auto resp = std::format(
+                    "insert into crops (name, humidity, brightness, is_legal, "
+                    "brain_damage) values('{}', {}, {}, {}, {});",
+                    args[1], args[2], args[3], args[4], args[5]);
+                pqxx::result res = query.exec(resp);
+                query.commit();
+                bot.getApi().sendMessage(message->chat->id, resp);
+            } catch (...) {
+                std::cout << "Database: failed to execute query: " << message->text << std::endl;
+            }
+        });
 
     try {
         std::cerr << GetCurrentTime()
